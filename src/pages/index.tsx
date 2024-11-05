b41157585ae2625bce9686cc69860592f7e75e78
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-11-05 17:42:01 +0800

    update description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Home`}
-      description="Description will go into a meta tag in <head />">
+      description="Hui Zhuan's personal site">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
